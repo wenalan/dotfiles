025c169db6a9f576bdccd19f2039879adcc3d245
Alan <wen_alan@126.com>
2023-12-20 14:07:49 +0000

    update scripts

--- enable_proxy.sh
+++ enable_proxy.sh
@@ -5,4 +5,4 @@ export ALL_PROXY="socks5://192.168.5.150:2013"
 
 # git
 mv ~/.ssh/config ~/.ssh/config_back
-ln -s ~/src/github/dotfiles/git_ssh_config ~/.ssh/config
+cp ~/src/github/dotfiles/git_ssh_config ~/.ssh/config
