99bf6947d5bfd76f8a8f463babbb5c52f4dd9595
Alan Wen <wen_alan@126.com>
2025-09-06 14:38:02 +0000

    update setup path

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 pushd ~
 
 mv .bashrc .bashrc_back
-ln -s ~/src/github/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
 
 mv .bash_aliases .bash_aliases_back
-ln -s ~/src/github/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 
 mv .tmux.conf .tmux.conf_back
-ln -s ~/src/github/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 mv .zshrc .zshrc_back
-ln -s ~/src/github/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 mv .vimrc .vimrc_back
-ln -s ~/src/github/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 mv .config/nvim/init.vim .config/nvim/init.vim_back
-ln -s ~/src/github/dotfiles/init.nvim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/init.nvim ~/.config/nvim/init.vim
 
 mv .emacs .emacs_back
-ln -s ~/src/github/dotfiles/.emacs ~/.emacs
+ln -s ~/dotfiles/.emacs ~/.emacs
 
 popd
 
